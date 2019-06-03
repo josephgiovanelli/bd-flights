3631a45b2cbd370768ac28896392a35649eeb14d
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-06-03 23:46:30 +0200

    update MachineLearningJob

--- spark-sql/src/main/scala/org/queue/bd/MachineLearningJob.scala
+++ spark-sql/src/main/scala/org/queue/bd/MachineLearningJob.scala
@@ -3,7 +3,7 @@ package org.queue.bd
 import java.io.PrintWriter
 
 import org.apache.hadoop.fs.{FileSystem, Path}
-import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, LogisticRegression}
+import org.apache.spark.ml.classification.{DecisionTreeClassificationModel, LogisticRegression, NaiveBayes}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import utils.TimeSlot
 
@@ -23,7 +23,16 @@ object MachineLearningJob {
       .option("header", "true")
       .option("delimiter", ",")
       .load("hdfs:/user/jgiovanelli/flights-dataset/raw/airports.csv")
-      .select("IATA_CODE", "LATITUDE", "LONGITUDE", "STATE")
+      .select("IATA_CODE", "AIRPORT", "LATITUDE", "LONGITUDE", "STATE")
+
+    //Airlines
+    val airlines = sqlContext.read
+      .format("csv")
+      .option("header", "true")
+      .option("delimiter", ",")
+      .load("hdfs:/user/jgiovanelli/flights-dataset/raw/airlines.csv")
+      .withColumnRenamed("IATA_CODE", "AIRLINES_IATA_CODE")
+      .withColumnRenamed("AIRLINE", "AIRLINES_NAME")
 
     //Flights
     val flights = sqlContext.read
@@ -33,22 +42,42 @@ object MachineLearningJob {
       .load("hdfs:/user/jgiovanelli/flights-dataset/raw/flights.csv")
       .filter(x => x.getAs[String]("CANCELLED") == "0" && x.getAs[String]("DIVERTED") == "0" &&
         x.getAs[String]("ORIGIN_AIRPORT").length == 3)
-      .select("ORIGIN_AIRPORT", "DESTINATION_AIRPORT", "AIRLINE", "SCHEDULED_DEPARTURE", "MONTH", "DISTANCE", "ARRIVAL_DELAY")
-      .cache()
+      .select("ORIGIN_AIRPORT", "DESTINATION_AIRPORT", "AIRLINE", "SCHEDULED_DEPARTURE", "MONTH", "DAY_OF_WEEK", "DISTANCE", "ARRIVAL_DELAY")
 
+    //Flights Join Origin Airport
     val trainDataTemp = flights
       .join(airports, flights("ORIGIN_AIRPORT") === airports("IATA_CODE"))
       .withColumnRenamed("IATA_CODE", "ORIGIN_IATA_CODE")
+      .withColumnRenamed("AIRPORT", "ORIGIN_AIRPORT_NAME")
       .withColumnRenamed("LATITUDE", "ORIGIN_LATITUDE")
       .withColumnRenamed("LONGITUDE", "ORIGIN_LONGITUDE")
       .withColumnRenamed("STATE", "ORIGIN_STATE")
 
-    val trainData = trainDataTemp
+    //Flights Join Destination Airport
+    val trainDataTemp2 = trainDataTemp
       .join(airports, trainDataTemp("DESTINATION_AIRPORT") === airports("IATA_CODE"))
       .withColumnRenamed("IATA_CODE", "DESTINATION_IATA_CODE")
+      .withColumnRenamed("AIRPORT", "DESTINATION_AIRPORT_NAME")
       .withColumnRenamed("LATITUDE", "DESTINATION_LATITUDE")
       .withColumnRenamed("LONGITUDE", "DESTINATION_LONGITUDE")
       .withColumnRenamed("STATE", "DESTINATION_STATE")
+
+    //Flights Join Airline
+    val trainDataTemp3 = trainDataTemp2
+      .join(airlines, trainDataTemp("AIRLINE") === airlines("AIRLINES_IATA_CODE"))
+
+    //Flights Join Airlines Statistics
+    import org.apache.spark.sql.Row
+    import org.apache.spark.sql.types.{StructType,StructField,StringType}
+    val airlinesStatisticsFile = sc.textFile("hdfs:/user/jgiovanelli/outputs/spark-sql/airlines/")
+    val schemaString = "STATISTIC_AIRLINES_NAME STATISTICS_AVG_DELAY"
+
+    val schema = StructType(schemaString.split(" ").map(fieldName => StructField(fieldName, StringType, true)))
+    val rowRDD = airlinesStatisticsFile.map(_.split(",")).map(e => Row(e(0), e(1)))
+    val airlinesStatistics = sqlContext.createDataFrame(rowRDD, schema)
+
+    val trainData = trainDataTemp3
+      .join(airlinesStatistics, trainDataTemp3("AIRLINES_NAME") === airlinesStatistics("STATISTIC_AIRLINES_NAME"))
       .cache()
 
     trainData.printSchema()
@@ -73,11 +102,11 @@ object MachineLearningJob {
 
     val extractLatitudeArea = sqlContext.udf.register("extractLatitudeArea",
       (latitude: Double) => {
-        if (latitude < 30) {
+        if (latitude < 30.0) {
           "0"
-        } else if (latitude < 40){
+        } else if (latitude < 40.0){
           "1"
-        } else if (latitude < 50){
+        } else if (latitude < 50.0){
           "2"
         } else {
           "3"
@@ -86,11 +115,11 @@ object MachineLearningJob {
 
     val extractLongitudeArea = sqlContext.udf.register("extractLongitudeArea",
       (longitude: Double) => {
-        if (longitude < -130) {
+        if (longitude < -130.0) {
           "0"
-        } else if (longitude < -110){
+        } else if (longitude < -110.0){
           "1"
-        } else if (longitude < -90){
+        } else if (longitude < -90.0){
           "2"
         } else {
           "3"
@@ -100,12 +129,20 @@ object MachineLearningJob {
 
     val stringToInt = sqlContext.udf.register("stringToInt", (s: String) => s.toInt)
     val intToDouble = sqlContext.udf.register("intToDouble", (i: Int) => i.toDouble)
-    val stringToDouble = sqlContext.udf.register("stringToDouble", (s: String) => s.toDouble)
+    val stringToDouble = sqlContext.udf.register("stringToDouble", (s: String) =>  {
+      val value = try { Some(s.toDouble) } catch { case _ => None }
+      value match {
+        case Some(e) => e
+        case _ => -80.0
+      }
+    })
+
 
     val trainData2 = trainData
       .withColumn("Airline", trainData("AIRLINE"))
       .withColumn("TimeSlot", extractTimeSlot(trainData("SCHEDULED_DEPARTURE")))
       .withColumn("Month", stringToInt(trainData("MONTH")))
+      .withColumn("DayOfWeek", stringToInt(trainData("DAY_OF_WEEK")))
       .withColumn("Distance", intToDouble(trainData("DISTANCE")))
       .withColumn("OriginLatitudeArea", stringToInt(extractLatitudeArea(stringToDouble(trainData("ORIGIN_LATITUDE")))))
       .withColumn("OriginLongitudeArea", stringToInt(extractLongitudeArea(stringToDouble(trainData("ORIGIN_LONGITUDE")))))
@@ -113,30 +150,44 @@ object MachineLearningJob {
       .withColumn("DestinationLatitudeArea", stringToInt(extractLatitudeArea(stringToDouble(trainData("DESTINATION_LATITUDE")))))
       .withColumn("DestinationLongitudeArea", stringToInt(extractLongitudeArea(stringToDouble(trainData("DESTINATION_LONGITUDE")))))
       .withColumn("DestinationState", trainData("DESTINATION_STATE"))
-      .withColumn("Delay2", intToDouble(extractDelayThreeshold(stringToInt(trainData("ARRIVAL_DELAY")))))
+      .withColumn("OriginAirportName", trainData("ORIGIN_AIRPORT_NAME"))
+      .withColumn("AverageAirlineDelay", stringToDouble(airlinesStatistics("STATISTICS_AVG_DELAY")))
+      .withColumn("Delay", intToDouble(extractDelayThreeshold(stringToInt(trainData("ARRIVAL_DELAY")))))
 
-    def balanceDataset(dataset: DataFrame): DataFrame = {
 
-      // Re-balancing (weighting) of records to be used in the logistic loss objective function
-      val numNegatives = dataset.filter(dataset("Delay2") === 0).count
-      val datasetSize = dataset.count
-      val balancingRatio = (datasetSize - numNegatives).toDouble / datasetSize
+    //Airports Statistics
+    import org.apache.spark.sql.Row
+    import org.apache.spark.sql.types.{StructType,StructField,StringType}
+    val airportsStatisticsFile = sc.textFile("hdfs:/user/jgiovanelli/outputs/spark-sql/airports/")
+    val schemaString2 = "AIRPORT TIME_SLOT AVG_TAXI_OUT"
 
-      val calculateWeights = sqlContext.udf.register("sample", (d: Double) => {
-        if (d == 0.0) {
-          1 * balancingRatio
-        }
-        else {
-          1 * (1.0 - balancingRatio)
-        }
-      })
+    val schema2 = StructType(schemaString2.split(" ").map(fieldName => StructField(fieldName, StringType, true)))
+    val rowRDD2 = airportsStatisticsFile.map(_.split(",")).map(e => Row(e(0), e(1), e(2)))
+    val airportsStatistics = sqlContext.createDataFrame(rowRDD2, schema2)
 
-      val weightedDataset = dataset
-        .withColumn("Delay", calculateWeights(dataset("Delay2")))
-      weightedDataset
-    }
+    val trainData3 = trainData2
+      .join(airportsStatistics, trainData2("OriginAirportName") === airportsStatistics("AIRPORT") && trainData2("TimeSlot") === airportsStatistics("TIME_SLOT"))
 
-    val trainDataFinal = balanceDataset(trainData2)
+    val trainDataFinal = trainData3
+      .withColumn("AverageTaxiOut", stringToDouble(trainData3("AVG_TAXI_OUT")))
+
+
+    // Re-balancing (weighting) of records to be used in the logistic loss objective function
+    /*val numPositeves = trainData2.filter(trainData2("Delay2") === 1).count
+    val datasetSize = trainData2.count
+    val balancingRatio = (datasetSize - numPositeves).toDouble / datasetSize
+
+    val calculateWeights = sqlContext.udf.register("sample", (d: Double) => {
+      if (d == 1) {
+        1 * balancingRatio
+      }
+      else {
+        1 * (1.0 - balancingRatio)
+      }
+    })*/
+
+   /* val trainDataFinal = trainData3
+      .withColumn("Delay", calculateWeights(trainData2("Delay2")))*/
 
     trainDataFinal.printSchema()
     trainDataFinal.show(5)
@@ -175,24 +226,28 @@ object MachineLearningJob {
     import org.apache.spark.ml.feature.VectorAssembler
 
     val assembler = new VectorAssembler()
-      .setInputCols(Array("AirlineIndex", "TimeSlotIndex", "Month", "DistanceBucketed", "OriginLatitudeArea",
-        "OriginLongitudeArea", "OriginStateIndex", "DestinationLatitudeArea", "DestinationLongitudeArea","DestinationStateIndex"))
+      .setInputCols(Array("AirlineIndex", "AverageAirlineDelay", "TimeSlotIndex", "Month", "DayOfWeek",
+        "DistanceBucketed", "OriginLatitudeArea", "OriginLongitudeArea", "OriginStateIndex", "AverageTaxiOut",
+        "DestinationLatitudeArea", "DestinationLongitudeArea","DestinationStateIndex"))
       .setOutputCol("features")
 
     import org.apache.spark.ml.classification.DecisionTreeClassifier
 
 
     val dt = new DecisionTreeClassifier()
-      .setMaxDepth(5)
-      .setMaxBins(350)
-
-    //val lr = new LogisticRegression().setMaxIter(10)
-
-    dt.setFeaturesCol("features")
+      .setMaxDepth(10)
+      .setMaxBins(54)
+      .setFeaturesCol("features")
       .setLabelCol("DelayIndex")
 
-    //lr.setFeaturesCol("features")
-    //  .setLabelCol("DelayIndex")
+    /*val lr = new LogisticRegression()
+      .setMaxIter(10)
+      .setFeaturesCol("features")
+      .setLabelCol("DelayIndex")*/
+
+    /*val nv = new NaiveBayes()
+      .setFeaturesCol("features")
+      .setLabelCol("DelayIndex")*/
 
     import org.apache.spark.ml.Pipeline
 
@@ -225,9 +280,10 @@ object MachineLearningJob {
 
     val trainMetrics = new BinaryClassificationMetrics(trainPredictionAndLabels.rdd)
 
-    println("Accuracy (Train): " +
-      trainResult.filter("prediction=DelayIndex").count.asInstanceOf[Double] /
-        trainResult.count.asInstanceOf[Double])
+    val trainAccuracy = trainResult.filter("prediction=DelayIndex").count.asInstanceOf[Double] / trainResult.count.asInstanceOf[Double]
+    //val weightedTrainAccuracy =  1 / (1 + (1 / (numPositeves / datasetSize) - 1) / (1 / balancingRatio - 1) * (1 / trainAccuracy - 1))
+    //val weightedTrainAccuracy = balancingRatio.asInstanceOf[Double] * trainAccuracy.asInstanceOf[Double] / ((balancingRatio.asInstanceOf[Double] - 1.0) * trainAccuracy.asInstanceOf[Double] + 1.0)
+    println("Accuracy (Train): " + trainAccuracy)
 
     println("Precision (Train): " +
       trainResult.filter("prediction=1 and DelayIndex=1").count.asInstanceOf[Double] /
@@ -248,9 +304,10 @@ object MachineLearningJob {
 
     val testMetrics = new BinaryClassificationMetrics(testPredictionAndLabels.rdd)
 
-    println("Accuracy (Test): " +
-      testResult.filter("prediction=DelayIndex").count.asInstanceOf[Double] /
-        testResult.count.asInstanceOf[Double])
+    val testAccuracy = testResult.filter("prediction=DelayIndex").count.asInstanceOf[Double] / testResult.count.asInstanceOf[Double]
+    //val weightedTestAccuracy =  1 / (1 + (1 / (numPositeves / datasetSize) - 1) / (1 / balancingRatio - 1) * (1 / testAccuracy - 1))
+    //val weightedTestAccuracy = balancingRatio.asInstanceOf[Double] * testAccuracy.asInstanceOf[Double] / ((balancingRatio.asInstanceOf[Double] - 1.0) * testAccuracy.asInstanceOf[Double] + 1.0)
+    println("Accuracy (Test): " + testAccuracy)
 
     println("Precision (Test): " +
       testResult.filter("prediction=1 and DelayIndex=1").count.asInstanceOf[Double] /
