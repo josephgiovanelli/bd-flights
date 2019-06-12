382292b366c11b916d4db22fd1b92582a81608ca
Joseph Giovanelli <joseph.giovanelli@studio.unibo.it>
2019-06-12 13:52:33 +0200

    Update ThirdJob.scala

--- spark/src/main/scala/org/queue/bd/ThirdJob.scala
+++ spark/src/main/scala/org/queue/bd/ThirdJob.scala
@@ -1,6 +1,7 @@
 package org.queue.bd
 
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.{HashPartitioner, SparkConf, SparkContext}
 import pojos.{Airline, Airport, Flight}
 import utils.TimeSlot
 import org.queue.bd.RDDUtils._
@@ -10,7 +11,9 @@ object ThirdJob {
 
   def main(args: Array[String]): Unit = {
 
-    val sc = new SparkContext(new SparkConf().setAppName("Spark AirlinesJob"))
+    val conf = new SparkConf().setAppName("Spark ThirdJob")
+    conf.registerKryoClasses(Array(classOf[Flight]))
+    val sc = new SparkContext(conf)
 
     //Loading Part
     val rddAirlines = sc.textFile("hdfs:/user/jgiovanelli/flights-dataset/clean/airlines")
@@ -23,50 +26,44 @@ object ThirdJob {
 
     val rddCommonFlights = sc.textFile("hdfs:/user/jgiovanelli/flights-dataset/clean/flights")
       .map(x => new Flight(x))
-      .cache()
+      //.map(x => ((x.getAirline, x.getOrigin_airport), (x.getScheduled_departure, x.getArrival_delay, x.getTaxi_out)))
+      //.partitionBy(new HashPartitioner(116))
+      .repartition(1)
+      .persist(StorageLevel.MEMORY_ONLY_SER)
+
+    //rddCommonFlights.count
+
+
 
     //Airlines Part
-    val rddFlightsForAirlines = rddCommonFlights
-      .map(x => (x.getAirline, x.getArrival_delay.toDouble))
-      .aggregateByKey((0.0, 0.0))((a, v) => (a._1 + v, a._2 + 1), (a1, a2) => (a1._1 + a2._1, a1._2 + a2._2))
-      .map({ case (k, v) => (k, v._1 / v._2) })
-
-    val broadcastRddAirlines = sc.broadcast(rddAirlines.collectAsMap())
-
-    val rddJoinedForAirlines = rddFlightsForAirlines
-      .map({ case (k, v) => (broadcastRddAirlines.value.get(k), v) })
-      .filter(_._1.isDefined)
-      .map({ case (k, v) => (k.get, v) })
-      .sortBy(_._2, ascending = false, numPartitions = 1)
-      .cache()
-
     def toCSVLineForAirlines(data: (String, Double)): String = data._1 + "," + data._2.toString
 
-    val rddResultForAirlines = rddJoinedForAirlines.map(x => toCSVLineForAirlines(x))
-    rddResultForAirlines.collect()
-    rddResultForAirlines.overwrite("hdfs:/user/jgiovanelli/outputs/spark/third-job/airlines")
+    rddCommonFlights
+      .map(x => (x.getAirline, x.getArrival_delay.toDouble))
+      //.map({ case (k, v) => (k._1, v._2.toDouble) })
+      .aggregateByKey((0.0, 0.0))((a, v) => (a._1 + v, a._2 + 1), (a1, a2) => (a1._1 + a2._1, a1._2 + a2._2))
+      .map({ case (k, v) => (k, v._1 / v._2) })
+      .join(rddAirlines)
+      .map({ case (k, v) => (v._2, v._1) })
+      .sortBy(_._2, ascending = false, numPartitions = 1)
+      .map(x => toCSVLineForAirlines(x))
+      .overwrite("hdfs:/user/jgiovanelli/outputs/spark/third-job/airlines")
 
     //Airports Part
-    val rddFlightsForAirports = rddCommonFlights
-      .map(x => ((x.getOrigin_airport, TimeSlot.getTimeSlot(x.getScheduled_departure).getDescription), x.getTaxi_out.toDouble))
-      .aggregateByKey((0.0, 0.0))((a, v) => (a._1 + v, a._2 + 1), (a1, a2) => (a1._1 + a2._1, a1._2 + a2._2))
-      .map({ case (k, v) => (k._1, (k._2, v._1 / v._2)) })
-
-    val broadcastRddAirports = sc.broadcast(rddAirports.collectAsMap())
-
-    val rddJoinedForAirports = rddFlightsForAirports
-      .map({ case (k, v) => (broadcastRddAirports.value.get(k), v) })
-      .filter(_._1.isDefined)
-      .map({ case (k, v) => ((k.get, v._1), v._2) })
-      .sortBy(_._2, ascending = false, numPartitions = 1)
-      .cache()
-
     def toCSVLineForAirports(data: ((String, String), Double)): String =
       data._1._1 + "," + data._1._2 + "," + data._2.toString
 
-    val rddResultForAirports = rddJoinedForAirports.map(x => toCSVLineForAirports(x))
-    rddResultForAirports.collect()
-    rddResultForAirports.overwrite("hdfs:/user/jgiovanelli/outputs/spark/third-job/airports")
+    rddCommonFlights
+      .map(x => ((x.getOrigin_airport, TimeSlot.getTimeSlot(x.getScheduled_departure).getDescription), x.getTaxi_out.toDouble))
+      //.map({ case (k, v) => ((k._2, TimeSlot.getTimeSlot(v._1).getDescription), v._3.toDouble) })
+      .aggregateByKey((0.0, 0.0))((a, v) => (a._1 + v, a._2 + 1), (a1, a2) => (a1._1 + a2._1, a1._2 + a2._2))
+      .map({ case (k, v) => (k._1, (k._2, v._1 / v._2)) })
+      //.partitionBy(new HashPartitioner(116))
+      .join(rddAirports)
+      .map({ case (k, v) => ((v._2, v._1._1), v._1._2) })
+      .sortBy(_._2, ascending = false, numPartitions = 1)
+      .map(x => toCSVLineForAirports(x))
+      .overwrite("hdfs:/user/jgiovanelli/outputs/spark/third-job/airports")
 
   }
 }
